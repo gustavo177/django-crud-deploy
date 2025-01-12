98b99d1e10ee8af7238b37e26d81434c9bfcf560
gustavo177 <gtvadf177@hotmail.com>
2025-01-11 19:00:24 -0500

    ALLOWED_HOSSTATIC_ROOT para que funcione de forma correcta el admin

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# TO-DO add the static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
